9f298885ac417cfe28cdc2e65adf45a30e2b5099
Alina Motrii <alinamotrii92@gmail.com>
2024-08-05 22:06:52 -0230

    create button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
+
 import "./App.css";
+import Button from "./components/Button/Button.jsx";
+
 
 function App() {
-  return <div className="App">
-    <h1>Hello World</h1>
-  </div>;
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Button isDisabled={isDisabled}></Button>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Button.module.scss"
+
+const Button = (props) => {
+  console.log(props);
+  
+  return (
+    <button className={styles.button}>  
+     
+    </button>
+  )
+}
+
+export default Button;
